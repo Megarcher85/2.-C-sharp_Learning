629bb60f1a92e6b57e037422892f4c42c7f5cc42
Artur Gubaydulin <megaarcher@mail.ru>
2024-02-06 20:21:20 +0500

    add Seminar1:Task3

---
+++ Seminar1/S1T3/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите трёхзначное число:");
+int N = int.Parse(Console.ReadLine());
+if (N < 0)
+{
+    N = -N;
+}
+int a = N / 100;
+int b = N % 10;
+int c = a + b;
+System.Console.Write("Сумма первой и последней цифры числа ");
+System.Console.Write(N);
+System.Console.Write(" равна: ");
+System.Console.Write(c);
+Console.ReadKey();
