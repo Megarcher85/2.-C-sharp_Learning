bbfa3dcfe1be6a8d2bf53982b5c688bb24593a3c
Megaarcher85 <103512044+Megarcher85@users.noreply.github.com>
2024-02-08 17:47:53 +0300

    Seminar2:HW_Task3

---
+++ Seminar2/S2HWTask3/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Введите любое двухзначное число");
+string stringNum = Console.ReadLine();
+int intNum = Convert.ToInt32(stringNum);
+// System.Console.WriteLine("Вы ввели число " + intNum);
+if (intNum<10 && intNum>99)
+{
+    Console.WriteLine("Повторите попытку");
+}
+else
+{
+    int a = intNum/10;
+    int b = intNum%10;
+    if (a==b)
+    {
+        Console.WriteLine($"Цыфры в числе {intNum} равны между собой");
+    }
+    else if (a>b)
+    {
+        Console.WriteLine($"Наибольшая цифра в числе {intNum}: {a}");
+    }
+        else
+    {
+        Console.WriteLine($"Наибольшая цифра в числе {intNum}: {b}");
+    }
+}
