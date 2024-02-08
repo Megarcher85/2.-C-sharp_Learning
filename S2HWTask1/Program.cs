649ccc9789aecd193b5949c4da2e243471c6975b
Megaarcher85 <103512044+Megarcher85@users.noreply.github.com>
2024-02-08 15:49:39 +0300

    Seminar2:HW_Task1

---
+++ S2HWTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАДАЧА 1
+System.Console.WriteLine("Введите любое число");
+string stringNum = Console.ReadLine();
+int intNum = Convert.ToInt32(stringNum);
+// System.Console.WriteLine("Вы ввели число " + intNum);
+if (intNum%7==0 && intNum%23==0)
+{
+    System.Console.WriteLine($"Число {intNum} кратно число 7 и 23.");
+}
+else
+{
+    System.Console.WriteLine($"Число {intNum} не кратно число 7 и 23.");
+}
